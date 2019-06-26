64ac2735f3e7fb9809e79f6ac7d33d2b042805ca
Fabio Pacheco <fabio.pacheco@complyadvantage.com>
2019-06-26 16:10:53 +0100

    Correct version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='requests-debugger',
 
-    version='0.0.2',
+    version='0.0.1',
 
     description='Prints queries executed on you projects along with line traceback.',
     long_description=long_description,
